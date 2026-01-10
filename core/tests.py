9b28e55a5b2b62473707e823e8cfa87cc3d49345
sagar kamankar <sagarkamankar26@gmail.com>
2026-01-10 22:30:59 +0530

    created testcaes

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Item
 
-# Create your tests here.
+class ItemAPITestCase(APITestCase):
+
+    def test_get_items_list(self):
+        """
+        Test GET /api/items/
+        """
+        response = self.client.get('/api/items/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_item(self):
+        """
+        Test POST /api/items/
+        """
+        data = {
+            "name": "Test Item",
+            "desc": "Test Description"
+        }
+        response = self.client.post('/api/items/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Item.objects.count(), 1)
+        self.assertEqual(Item.objects.first().name, "Test Item")
