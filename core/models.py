0b6c95f414ef8da5d780773ed453ddafc8b27aa1
sagar kamankar <sagarkamankar26@gmail.com>
2026-01-10 22:02:35 +0530

    changes in models file

--- core/models.py
+++ core/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    naam=models.CharField(max_length=100)
+    name=models.CharField(max_length=100)
     desc=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.naam
+        return self.name
