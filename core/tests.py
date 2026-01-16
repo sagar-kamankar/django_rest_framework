123426834aee3cbdeb04f105cd196ccd44cc77ca
sagar kamankar <sagarkamankar26@gmail.com>
2026-01-16 17:13:41 +0530

    changes in tests.py file

--- core/tests.py
+++ core/tests.py
@@ -16,7 +16,7 @@ class ItemAPITestCase(APITestCase):
         Test POST /api/items/
         """
         data = {
-            "name": "Test Item",
+            "name": "test Item",
             "desc": "Test Description"
         }
         response = self.client.post('/api/items/', data, format='json')
